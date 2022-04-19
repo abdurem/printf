62ee6fe61324c13216adf9c38067506d4bf35605
abdurem <76563178+abdurem@users.noreply.github.com>
2022-04-19 19:43:50 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -3,12 +3,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/**
- * _putchar - prints given character
- * @c: character to print.
- *
- * Return: number of characters printed, -1 if error
- */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
