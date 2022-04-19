290c31d983ccfb18940c4d1946d2d48965a735e3
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2022-04-19 23:38:56 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -4,12 +4,11 @@
 #include <unistd.h>
 
 /**
- * _putchar - prints given character 
+ * _putchar - prints given character
  * @c: character to print.
  *
- * Return: number of characters printed, -1 if erorr
+ * Return: number of characters printed, -1 if error
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
